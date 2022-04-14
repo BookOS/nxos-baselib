d3aa3ae64a3b7b04c20f2564c8606253e674cfc2
hzc1998 <2323168280@qq.com>
2022-04-14 19:13:46 +0800

    feat: update hub

--- src/include/nxbase/hub.h
+++ src/include/nxbase/hub.h
@@ -14,6 +14,8 @@
 
 #include <nxbase/xbook.h>
 
+typedef void * NX_HubHandler;
+
 typedef struct NX_Hub
 {
     int hubData;
@@ -30,7 +32,10 @@ NX_Error NX_HubRegister(const char *name, NX_Size maxClient);
 NX_Error NX_HubUnregister(const char *name);
 NX_Error NX_HubCallParam(NX_Hub *hub, NX_HubParam *param, NX_Size *retVal);
 NX_Error NX_HubCallParamName(const char *name, NX_HubParam *param, NX_Size *retVal);
-NX_Error NX_HubReturn(NX_Size retVal);
+NX_Error NX_HubReturn(NX_Size retVal, NX_Error retErr);
 NX_Error NX_HubPoll(NX_HubParam *param);
+NX_Error NX_HubDispatch(NX_HubParam *param, NX_HubHandler handlerTable[], NX_Size tableSize);
+
+NX_Error NX_HubLoop(NX_HubHandler handlerTable[], NX_Size tableSize);
 
 #endif  /* __NXBASE_HUB__ */
--- src/ipc/hub.c
+++ src/ipc/hub.c
@@ -32,12 +32,72 @@ NX_Error NX_HubCallParamName(const char *name, NX_HubParam *param, NX_Size *retV
     return NX_Syscall3(NX_API_HubCallParamName, name, param, retVal);
 }
 
-NX_Error NX_HubReturn(NX_Size retVal)
+NX_Error NX_HubReturn(NX_Size retVal, NX_Error retErr)
 {
-    return NX_Syscall1(NX_API_HubReturn, retVal);
+    return NX_Syscall2(NX_API_HubReturn, retVal, retErr);
 }
 
 NX_Error NX_HubPoll(NX_HubParam *param)
 {
     return NX_Syscall1(NX_API_HubPoll, param);
+}
+
+typedef NX_UArch (*NX_HubHandlerWithArg) (NX_UArch, NX_UArch, NX_UArch, NX_UArch, NX_UArch, NX_UArch, NX_UArch, NX_UArch);
+
+NX_PRIVATE NX_HubHandler NX_HubGetHandler(NX_U32 api, NX_HubHandler handlerTable[], NX_Size tableSize)
+{
+    NX_HubHandler handler = NX_NULL;
+
+    if (api < tableSize)
+    {
+        handler = handlerTable[api];
+    }
+    return handler;
+}
+
+NX_Error NX_HubDispatch(NX_HubParam *param, NX_HubHandler handlerTable[], NX_Size tableSize)
+{
+    NX_Error errVal = NX_EOK;
+    NX_UArch retVal = 0;
+    NX_HubHandlerWithArg handler;
+
+    if (!param || !handlerTable || !tableSize)
+    {
+        return NX_EINVAL;
+    }
+
+    handler = (NX_HubHandlerWithArg)NX_HubGetHandler(param->api, handlerTable, tableSize);
+    if (handler == NX_NULL)
+    {
+        errVal = NX_ENORES;
+    }
+    else
+    {
+        retVal = handler(param->args[0], param->args[1], param->args[2], param->args[3],
+                         param->args[4], param->args[5], param->args[6], param->args[7]);
+    }
+
+    return NX_HubReturn(retVal, errVal);
+}
+
+NX_Error NX_HubLoop(NX_HubHandler handlerTable[], NX_Size tableSize)
+{
+    NX_Error err;
+    NX_HubParam param;
+    while (1)
+    {
+        err = NX_HubPoll(&param);
+        if (err == NX_ENORES)
+        {
+            return NX_ENORES;
+        }
+        else if (err == NX_EOK)
+        {
+            err = NX_HubDispatch(&param, handlerTable, tableSize);
+            if (err != NX_EOK)
+            {
+                return err;
+            }
+        }
+    }
 }
